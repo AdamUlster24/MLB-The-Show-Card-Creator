d37da97e687d863ff4df092db7c1c37d1ebcc346
unknown <veggiemagic1124@gmail.com>
2021-09-26 16:57:26 -0400

    changed file download name

--- JS/Download.js
+++ JS/Download.js
@@ -1,6 +1,6 @@
 function download() {
     domtoimage.toBlob(document.querySelector('.container-of-image-with-text'))
     .then(function (blob) {
-        window.saveAs(blob, playerName + "card art.png");
+        window.saveAs(blob, document.getElementById("player") + "card art.png");
     });
 }
